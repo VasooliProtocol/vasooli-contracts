ffdf4c63a1241e5c695d1318f896afb846cfb9cc
Akhil Bharti <akhil.bh96@gmail.com>
2018-10-18 18:48:25 +0530

    minor changes

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,6 +1,7 @@
+pragma solidity ^0.4.24;
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-solidity/master/contracts/token/ERC20/ERC20.sol";
 
-contract TestToken is MintableToken {
+contract TestToken is ERC20 {
     string public constant name = "Test Token";
     string public constant symbol = "TT";
     uint8 public constant decimals = 18;
